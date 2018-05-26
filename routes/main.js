0874becd7ea2d9f8850cff73477baa4eca363439
JoeSuzuki <joesuzuki516@gmail.com>
2018-05-26 14:06:34 -0400

    main view

---
+++ routes/main.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var obj = {tagArray: ["Art", "Science", "Psychology", "Programming", "Education", "Family"]}
+    res.render('info', obj);
+});
+
+module.exports = router;
