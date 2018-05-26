2f7fc07ed34b0a220555f953196df1aab9203ed2
JoeSuzuki <joesuzuki516@gmail.com>
2018-05-26 16:02:00 -0400

    finished profile US

--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,8 @@ router.get('/', function(req, res, next) {
     var obj = {
         friendArray: ["Rod", "Joe", "Bill"],
         name: "Name",
-        tagArray: ["art", "engineer", "cs"]
+        tagArray: ["art", "engineer", "cs"],
+        image: "https://lh3.googleusercontent.com/YXQRKv60cfiwFXfG4cULrNIWjh4YV2Ey3-8ua0VUlzWpsVCaQXZLdTnNJmgHG6-yAcA=h300"
     }
 
     res.render('profile', obj);
