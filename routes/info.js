4dd860d63ba407a60125c1fb51d8f3feedbca948
JoeSuzuki <joesuzuki516@gmail.com>
2018-05-26 13:18:02 -0400

    obj array in info.js

--- routes/info.js
+++ routes/info.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('info', { title: 'Express' });
+  var obj = {tagArray: ["Art", "Science", "Business"]}
+  res.render('info', obj);
 });
 
 module.exports = router;
